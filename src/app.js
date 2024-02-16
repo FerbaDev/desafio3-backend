54ded5d0b422bc7ff0b8b413fca54ccdd9a3e45a
FerbaDev <elferbarron1@gmail.com>
2024-02-16 18:54:03 -0300

    tratando de importar productManager

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
-import express from "express";
+const express = require("express");
 const app = express();
 const PUERTO = 8080;
+//traemos el productManager
+const ProductManager = require("./controllers/productManager.js");
+//instanciamos
+const productManager = new ProductManager("./models/products.json");
 
 //configuramos middleware
 app.use(express.json());
@@ -13,3 +17,6 @@ app.get("/", (req, res) => {
 app.listen(PUERTO, () => {
   console.log(`escuchando en http://localhost:${PUERTO}`);
 });
+
+/////////////////////////////////////////////
+// dejé en el minuto 14 del after
